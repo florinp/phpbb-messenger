798c64daab92fcc16671559c610388f96eb2da64
Florin Pavel <florinp94@gmail.com>
2016-03-25 23:52:11 +0000

    v1.0.6-dev released: new design and send files improvement

--- controller/main.php
+++ controller/main.php
@@ -8,237 +8,247 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class main
 {
 
-	protected $config;
-	protected $helper;
-	protected $template;
-	protected $user;
-	protected $model;
-	protected $request;
-	protected $notification_manager;
-	protected $emojione;
+    protected $config;
+    protected $helper;
+    protected $template;
+    protected $user;
+    protected $model;
+    protected $request;
+    protected $notification_manager;
+    protected $emojione;
+    protected $upload;
+    protected $download;
 
-	public function __construct(\phpbb\config\config $config, \phpbb\controller\helper $helper, \phpbb\template\template $template, \phpbb\request\request $request, \phpbb\user $user, \florinp\messenger\models\main_model $model, \phpbb\notification\manager $notification_manager, \florinp\messenger\libs\emojione $emojione)
-	{
-		$this->config = $config;
-		$this->helper = $helper;
-		$this->template = $template;
-		$this->request = $request;
-		$this->user = $user;
-		$this->model = $model;
-		$this->notification_manager = $notification_manager;
-		$this->emojione = $emojione;
-	}
+    public function __construct(
+        \phpbb\config\config $config,
+        \phpbb\controller\helper $helper,
+        \phpbb\template\template $template,
+        \phpbb\request\request $request,
+        \phpbb\user $user,
+        \florinp\messenger\models\main_model $model,
+        \phpbb\notification\manager $notification_manager,
+        \florinp\messenger\libs\emojione $emojione,
+        \florinp\messenger\libs\upload $upload,
+        \florinp\messenger\libs\download $download
+    )
+    {
+        $this->config = $config;
+        $this->helper = $helper;
+        $this->template = $template;
+        $this->request = $request;
+        $this->user = $user;
+        $this->model = $model;
+        $this->notification_manager = $notification_manager;
+        $this->emojione = $emojione;
+        $this->upload = $upload;
+        $this->download = $download;
+    }
 
-	public function handle()
-	{
-	}
+    public function handle()
+    {
+    }
 
-	public function index()
-	{
+    public function index()
+    {
 
-	}
+    }
 
-	public function publish()
-	{
-		/* AJAX check  */
-		$http_request = $this->request->server('HTTP_X_REQUESTED_WITH');
-		if (empty($http_request) && strtolower($http_request) != 'xmlhttprequest') {
-			return new Response("The request is invalid", 500);
-		}
+    public function publish()
+    {
+        /* AJAX check  */
+        $http_request = $this->request->server('HTTP_X_REQUESTED_WITH');
+        if (empty($http_request) && strtolower($http_request) != 'xmlhttprequest') {
+            return new Response("The request is invalid", 500);
+        }
 
-		$text = $this->request->variable('text', '', true);
-		$receiver_id = $this->request->variable('receiver_id', 0);
-		$sender_id = $this->user->data['user_id'];
+        $text = $this->request->variable('text', '', true);
+        $receiver_id = $this->request->variable('receiver_id', 0);
+        $sender_id = $this->user->data['user_id'];
 
 
-		if ($receiver_id != 0 && trim($text) != '')
-		{
-			$text = htmlspecialchars($text);
-			$text = str_replace(array("\n", "\r"), '', $text);
+        if ($receiver_id != 0 && trim($text) != '') {
+            $text = htmlspecialchars($text);
+            $text = str_replace(array("\n", "\r"), '', $text);
 
-			$message = array(
-				'sender_id' => $sender_id,
-				'receiver_id' => $receiver_id,
-				'text' => $text,
-				'sentAt' => time()
-			);
+            $message = array(
+                'sender_id' => $sender_id,
+                'receiver_id' => $receiver_id,
+                'text' => $text,
+                'sentAt' => time()
+            );
 
-			if ($id = $this->model->sendMessage($message))
-			{
-				$lastMessage = $this->model->getMessageById($id);
-				$response = array('success' => true, 'message' => $lastMessage);
-			} else {
-				$response = array(
-					'succes' => false,
-					'error' => 'An error has been ocurred!'
-				);
-			}
-		}
+            if ($id = $this->model->sendMessage($message)) {
+                $lastMessage = $this->model->getMessageById($id);
+                $response = array('success' => true, 'message' => $lastMessage);
+            } else {
+                $response = array(
+                    'succes' => false,
+                    'error' => 'An error has been ocurred!'
+                );
+            }
+        }
 
-		return new JsonResponse($response, 200);
-	}
+        return new JsonResponse($response, 200);
+    }
 
-	public function getFile($id)
-	{
-			global $phpbb_root_path;
-			$uploadDir = $phpbb_root_path.'store/messenger/files';
-			$id = explode('_', $id)[1];
-			$file = $this->model->getFileById($id);
-			if ($file && is_file($uploadDir.'/'.$file['file'])) {
-				header('Content-Type: application/csv');
-				header('Content-Disposition: attachment; filename='.$file['file']);
-				header('Pragma: no-cache');
-				readfile($uploadDir.'/'.$file['file']);
-			} else {
-				return new Response("The request is invalid", 500);
-			}
-	}
+    public function getFile($id)
+    {
+        $id = explode('_', $id)[1];
+        $file = $this->model->getFileById($id);
+        $this->download->setFile($file['file']);
+        $this->download->sendDownload();
+    }
 
-	public function sendFile()
-	{
-		global $phpbb_root_path;
+    public function sendFile()
+    {
+        $receiver_id = $this->request->variable('receiver_id', 0);
+        $sender_id = $this->user->data['user_id'];
 
-		$receiver_id = $this->request->variable('receiver_id', 0);
-		$sender_id = $this->user->data['user_id'];
+        $file = $this->request->file('file');
+        if ($receiver_id != 0 && !empty($file)) {
+            if ($file['error'] == 0) {
+                $this->upload->file($file);
+                $this->upload->set_allowed_mime_types(array(
+                    'image/gif',
+                    'image/jpeg',
+                    'image/png',
+                    'application/pdf',
+                    'application/x-rar-compressed',
+                    'application/zip',
+                    'application/x-7z-compressed',
+                    'text/plain'
+                ));
+                $results = $this->upload->upload();
+                if (isset($results['errors']) && count($results['errors'])) {
+                    $response = array(
+                        'success' => false,
+                        'errors' => $results['errors']
+                    );
+                } else {
+                    $data = array(
+                        'sender_id' => $sender_id,
+                        'receiver_id' => $receiver_id,
+                        'fileName' => $results['original_filename'],
+                        'file' => $results['filename'],
+                        'type' => $results['mime'],
+                    );
+                    if ($id = $this->model->sendFile($data)) {
+                        $lastFile = $this->model->getFileById($id);
+                        $response = array(
+                            'success' => true,
+                            'file' => $lastFile
+                        );
+                    } else {
+                        $response = array(
+                            'succes' => false,
+                            'error' => 'An error has been ocurred!'
+                        );
+                    }
+                }
+            } else {
+                $response = array(
+                    'succes' => false,
+                    'error' => $file['error']
+                );
+            }
+        } else {
+            $response = array(
+                'succes' => false,
+                'error' => 'An error has been ocurred!'
+            );
+        }
 
-		$uploadDir = $phpbb_root_path.'store/messenger/files';
-		$file = $this->request->file('file');
-		if ($receiver_id != 0 && !empty($file)) {
-			if ($file['error'] == 0) {
-				$extension = pathinfo($file['name'], PATHINFO_EXTENSION);
-				$fileName = $file['name'];
-				$newName = md5(time().''.$fileName).'.'.$extension;
-				$type = $file['type'];
+        return new JsonResponse($response, 200);
+    }
 
-				if (move_uploaded_file($file['tmp_name'], $uploadDir.'/'.$newName)) {
-					$data = array(
-						'sender_id' => $sender_id,
-						'receiver_id' => $receiver_id,
-						'fileName' => $fileName,
-						'file' => $newName,
-						'type' => $type
-					);
-					if ($id = $this->model->sendFile($data)) {
-						$lastFile = $this->model->getFileById($id);
-						$response = array('success' => true, 'file' => $lastFile);
-					} else {
-						$response = array(
-							'succes' => false,
-							'error' => 'An error has been ocurred!'
-						);
-					}
-				} else {
-					$response = array(
-						'succes' => false,
-						'error' => 'An error has been ocurred!'
-					);
-				}
-			} else {
-				$response = array(
-					'succes' => false,
-					'error' => $file['error']
-				);
-			}
-		} else {
-			$response = array(
-				'succes' => false,
-				'error' => 'An error has been ocurred!'
-			);
-		}
+    public function load()
+    {
+        /* AJAX check  */
+        $http_request = $this->request->server('HTTP_X_REQUESTED_WITH');
+        if (empty($http_request) && strtolower($http_request) != 'xmlhttprequest') {
+            return new Response("The request is invalid", 500);
+        }
 
-		return new JsonResponse($response, 200);
-	}
+        $friend_id = $this->request->variable('friend_id', 0);
 
-	public function load()
-	{
-		/* AJAX check  */
-		$http_request = $this->request->server('HTTP_X_REQUESTED_WITH');
-		if (empty($http_request) && strtolower($http_request) != 'xmlhttprequest') {
-			return new Response("The request is invalid", 500);
-		}
+        if ($friend_id > 0) {
+            $messages = $this->model->getMessages($friend_id);
+            return new JsonResponse($messages, 200);
+        }
+        return new JsonResponse(array('success' => false, 'error' => 'The request is invalid'), 200);
+    }
 
-		$friend_id = $this->request->variable('friend_id', 0);
+    public function updateMessages()
+    {
+        /* AJAX check  */
+        $http_request = $this->request->server('HTTP_X_REQUESTED_WITH');
+        if (empty($http_request) && strtolower($http_request) != 'xmlhttprequest') {
+            return new Response("The request is invalid", 500);
+        }
 
-		if ($friend_id > 0) {
-			$messages = $this->model->getMessages($friend_id);
-			return new JsonResponse($messages, 200);
-		}
-		return new JsonResponse(array('success' => false, 'error' => 'The request is invalid'), 200);
-	}
+        $friend_id = $this->request->variable('friend_id', 0);
+        if ($friend_id > 0) {
+            $newVal = $this->model->updateMessagesStatus($friend_id);
+            return new JsonResponse(array('success' => true, 'newVal' => $newVal), 200);
+        }
+        return new JsonResponse(array('success' => false), 200);
+    }
 
-	public function updateMessages()
-	{
-		/* AJAX check  */
-		$http_request = $this->request->server('HTTP_X_REQUESTED_WITH');
-		if (empty($http_request) && strtolower($http_request) != 'xmlhttprequest') {
-			return new Response("The request is invalid", 500);
-		}
+    public function checkForNewMessages()
+    {
+        /* AJAX check  */
+        $http_request = $this->request->server('HTTP_X_REQUESTED_WITH');
+        if (empty($http_request) && strtolower($http_request) != 'xmlhttprequest') {
+            return new Response("The request is invalid", 500);
+        }
 
-		$friend_id = $this->request->variable('friend_id', 0);
-		if ($friend_id > 0)
-		{
-			$newVal = $this->model->updateMessagesStatus($friend_id);
-			return new JsonResponse(array('success' => true, 'newVal' => $newVal), 200);
-		}
-		return new JsonResponse(array('success' => false), 200);
-	}
+        $friend_id = $this->request->variable('friend_id', 0);
+        if ($friend_id > 0) {
+            $messages = $this->model->getInboxFromId($friend_id);
+            return new JsonResponse(array('success' => true, 'messages' => $messages), 200);
+        }
+        return new JsonResponse(array('success' => false), 200);
+    }
 
-	public function checkForNewMessages()
-	{
-		/* AJAX check  */
-		$http_request = $this->request->server('HTTP_X_REQUESTED_WITH');
-		if (empty($http_request) && strtolower($http_request) != 'xmlhttprequest') {
-			return new Response("The request is invalid", 500);
-		}
+    public function getFriends()
+    {
+        /* AJAX check  */
+        $http_request = $this->request->server('HTTP_X_REQUESTED_WITH');
+        if (empty($http_request) && strtolower($http_request) != 'xmlhttprequest') {
+            return new Response("The request is invalid", 500);
+        }
 
-		$friend_id = $this->request->variable('friend_id', 0);
-		if ($friend_id > 0)
-		{
-			$messages = $this->model->getInboxFromId($friend_id);
-			return new JsonResponse(array('success' => true, 'messages' => $messages), 200);
-		}
-		return new JsonResponse(array('success' => false), 200);
-	}
+        $friends = $this->model->getFriends();
+        $friends_online = array_filter($friends, function ($friend) {
+            return $friend['user_status'] != 0;
+        });
 
-	public function getFriends()
-	{
-		/* AJAX check  */
-		$http_request = $this->request->server('HTTP_X_REQUESTED_WITH');
-		if (empty($http_request) && strtolower($http_request) != 'xmlhttprequest') {
-			return new Response("The request is invalid", 500);
-		}
+        $response = array(
+            'friends_online' => count($friends_online),
+            'friends_list' => $friends
+        );
 
-		$friends = $this->model->getFriends();
-		$friends_online = array_filter($friends, function($friend) {
-			return $friend['user_status'] != 0;
-		});
+        return new JsonResponse($response, 200);
+    }
 
-		$response = array(
-			'friends_online' => count($friends_online),
-			'friends_list' => $friends
-		);
+    public function getEmoticons()
+    {
+        $emoticons = \florinp\messenger\libs\emojione::$ascii_replace;
+        $eicons = array();
+        foreach ($emoticons as $code => $value) {
+            $eicons[$value] = $code;
+        }
 
-		return new JsonResponse($response, 200);
-	}
+        $response = array();
+        foreach ($eicons as $emoticon) {
+            $item = array();
+            $item['code'] = $emoticon;
+            $item['image'] = $this->emojione->toImage($emoticon);
 
-	public function getEmoticons()
-	{
-		$emoticons = \florinp\messenger\libs\emojione::$ascii_replace;
-		$eicons = array();
-		foreach ($emoticons as $code => $value) {
-			$eicons[$value] = $code;
-		}
+            $response[] = $item;
+        }
 
-		$response = array();
-		foreach ($eicons as $emoticon) {
-			$item = array();
-			$item['code'] = $emoticon;
-			$item['image'] = $this->emojione->toImage($emoticon);
-
-			$response[] = $item;
-		}
-
-		return new JsonResponse($response, 200);
-	}
+        return new JsonResponse($response, 200);
+    }
 
 }
--- libs/database.php
+++ libs/database.php
@@ -6,87 +6,98 @@ use PDO;
 
 class database extends \PDO
 {
-	protected $extension_manager;
+    protected $extension_manager;
 
-	public function __construct()
-	{
-		global $phpbb_root_path;
+    public function __construct()
+    {
+        global $phpbb_root_path;
 
-		$database = $phpbb_root_path.'store/messenger.db';
-		parent::__construct('sqlite:'.$database);
-		parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
+        $database = $phpbb_root_path . 'store/messenger.db';
+        parent::__construct('sqlite:' . $database);
+        parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
 
-	/**
-	 * @param string $sql
-	 */
-	public function select($sql, $array = array(), $fetchMode = PDO::FETCH_ASSOC)
-	{
-		$sth = $this->prepare($sql);
-		foreach ($array as $key => $value)
-		{
-			$sth->bindValue("$key", $value);
-		}
+    /**
+     * @param string $sql
+     * @return array
+     */
+    public function select($sql, $array = array(), $fetchMode = PDO::FETCH_ASSOC)
+    {
+        $sth = $this->prepare($sql);
+        foreach ($array as $key => $value) {
+            $sth->bindValue("$key", $value);
+        }
 
-		$sth->execute();
-		return $sth->fetchAll($fetchMode);
-	}
+        $sth->execute();
+        return $sth->fetchAll($fetchMode);
+    }
 
-	/**
-	 * @param string $table
-	 */
-	public function insert($table, $data)
-	{
-		//ksort($data);
+    /**
+     * @param string $table
+     * @return bool
+     */
+    public function insert($table, $data)
+    {
+        //ksort($data);
 
-		$fieldNames = implode('`, `', array_keys($data));
-		$fieldValues = ':'.implode(', :', array_keys($data));
+        $fieldNames = implode('`, `', array_keys($data));
+        $fieldValues = ':' . implode(', :', array_keys($data));
 
-		$sth = $this->prepare("INSERT INTO $table (`$fieldNames`) VALUES ($fieldValues)");
+        $sth = $this->prepare("INSERT INTO $table (`$fieldNames`) VALUES ($fieldValues)");
 
-		foreach ($data as $key => $value)
-		{
-			$sth->bindValue(":$key", $value);
-		}
+        foreach ($data as $key => $value) {
+            $sth->bindValue(":$key", $value);
+        }
 
-		return $sth->execute();
-	}
+        return $sth->execute();
+    }
 
-	public function update($table, $data, $where)
-	{
-		ksort($data);
+    /**
+     * @param string $table
+     * @param array $data
+     * @param string $where
+     * @return bool
+     */
+    public function update($table, $data, $where)
+    {
+        ksort($data);
 
-		$fieldDetails = NULL;
-		foreach ($data as $key=> $value) {
-			$fieldDetails .= "`$key`=:$key,";
-		}
-		$fieldDetails = rtrim($fieldDetails, ',');
+        $fieldDetails = NULL;
+        foreach ($data as $key => $value) {
+            $fieldDetails .= "`$key`=:$key,";
+        }
+        $fieldDetails = rtrim($fieldDetails, ',');
 
-		$sth = $this->prepare("UPDATE $table SET $fieldDetails WHERE $where");
+        $sth = $this->prepare("UPDATE $table SET $fieldDetails WHERE $where");
 
-		foreach ($data as $key => $value) {
-			$sth->bindValue(":$key", $value);
-		}
+        foreach ($data as $key => $value) {
+            $sth->bindValue(":$key", $value);
+        }
 
-		return $sth->execute();
-	}
+        return $sth->execute();
+    }
 
-	public function delete($table, $where, $limit = 1)
-	{
-		return $this->exec("DELETE FROM $table WHERE $where LIMIT $limit");
-	}
+    /**
+     * @param string $table
+     * @param string $where
+     * @param int $limit
+     * @return int
+     */
+    public function delete($table, $where, $limit = 1)
+    {
+        return $this->exec("DELETE FROM $table WHERE $where LIMIT $limit");
+    }
 
-	protected function find($path, $prefix, $suffix)
-	{
-		$finder = $this->extension_manager->get_finder();
+    protected function find($path, $prefix, $suffix)
+    {
+        $finder = $this->extension_manager->get_finder();
 
-		return $finder
-		->set_extensions(array('phpbb/messenger'))
-		->prefix($prefix)
-		->suffix($suffix)
-		->core_path("$path/")
-		->extension_directory("/$path")
-		->find()
-		;
-	}
+        return $finder
+            ->set_extensions(array('phpbb/messenger'))
+            ->prefix($prefix)
+            ->suffix($suffix)
+            ->core_path("$path/")
+            ->extension_directory("/$path")
+            ->find();
+    }
 }
